d18b4006f720701bc1ab08affd71bd2595f1d7db
Matias Ruiz <matiasruiz6899@gmail.com>
2020-08-24 13:44:24 -0300

    removed js added required

--- public/main.js
+++ public/main.js
@@ -1,11 +1,2 @@
-document.getElementById("msj").addEventListener("click", function (event) {
-    event.preventDefault()
-});
 
-document.getElementById("email").addEventListener("click", function (event) {
-    event.preventDefault()
-}); 
 
-document.getElementById("nombre").addEventListener("click", function (event) {
-    event.preventDefault()
-});
