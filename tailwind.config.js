f0349ec8c948b7af5db267a16563c46a7c65e7ac
matiasruiz21 <65144095+matiasruiz21@users.noreply.github.com>
2020-08-23 21:07:23 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        navyblue: {
+          100: '#9494FF',
+          200: '#6666FF',
+          300:'#3939FF',
+          400:'#0B0BFF',
+          500:'#0000DC',
+          600:'#0000AE',
+          700:'#000080',
+      },
+      summerblue: {
+        100:'#D4F1F4',
+        200:'#75E6DA',
+        300:'#189AB4',
+        400:'#05445E',
+      }
+    },
+      fontFamily: {
+        paytech: ['Chakra Petch']
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
+
