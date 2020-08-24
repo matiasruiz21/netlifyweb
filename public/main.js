bad10141542fedad33f78554be0061361ffd5a8d
Matias Ruiz <matiasruiz6899@gmail.com>
2020-08-24 13:17:45 -0300

    added js

---
+++ public/main.js
@@ -0,0 +1,11 @@
+document.getElementById("msj").addEventListener("click", function (event) {
+    event.preventDefault()
+});
+
+document.getElementById("email").addEventListener("click", function (event) {
+    event.preventDefault()
+}); 
+
+document.getElementById("nombre").addEventListener("click", function (event) {
+    event.preventDefault()
+});
